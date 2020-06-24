d0c3772dafcf5985c4033748f9ab5a2ebfe74c70
Silva404 <brenosilva0252@gmail.com>
2020-06-23 22:12:29 -0300

    routes showing bugs again

--- routes.js
+++ routes.js
@@ -1,20 +1,26 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/', (req,res) => {
+routes.get('/', (req, res) => {
     res.render('affiliates/index')
 })
 
-routes.get('/teachers', (req,res) => {
-    res.render('affiliates/index')
+routes.get('/teachers', (req, res) => {
+    return res.render('affiliates/index')
 })
 
-routes.get('/teachers/create', (req,res) => {
-    res.render('affiliates/create')
+routes.post('/teachers', (req, res) => {
+    return res.send(req.body)
 })
 
-routes.get('/students', (req,res) => {
-    res.render('students')
+routes.get('/teachers/create', (req, res) => {
+    return res.render('affiliates/create')
 })
+
+
+
+// routes.get('/students', (req, res) => {
+//     return res.render('students')
+// })
 
 module.exports = routes
