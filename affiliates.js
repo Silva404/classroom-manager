48e49835326bc93607e0f9ee86f1e8012c2a3e9f
Silva404 <brenosilva0252@gmail.com>
2020-06-23 23:40:28 -0300

    finalizando o dia feliz

---
+++ affiliates.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+exports.post = (req, res) => {
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == '') {
+            return res.send('Please fill all the fields!')
+        }
+    }
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) return res.send('Error ocurred while writing data.json')
+
+        return res.redirect('/teachers')
+    })    
+}
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const affiliates = require('./affiliates')
 
 routes.get('/', (req, res) => {
     res.render('affiliates/index')
@@ -9,18 +10,14 @@ routes.get('/teachers', (req, res) => {
     return res.render('affiliates/index')
 })
 
-routes.post('/teachers', (req, res) => {
-    return res.send(req.body)
-})
+routes.post('/teachers', affiliates.post)
 
 routes.get('/teachers/create', (req, res) => {
     return res.render('affiliates/create')
 })
 
-
-
-// routes.get('/students', (req, res) => {
-//     return res.render('students')
-// })
+routes.get('/students', (req, res) => {
+    return res.render('students')
+})
 
 module.exports = routes
